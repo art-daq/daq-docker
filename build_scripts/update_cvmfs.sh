664df0a006c2891a3016f95b8150990c3597d996
Eric Flumerfelt <eflumerf@fnal.gov>
2024-11-11 16:20:01 -0600

    Download scripts, rsync artdaq_spack area before build for consistency

--- build_scripts/update_cvmfs.sh
+++ build_scripts/update_cvmfs.sh
@@ -1,7 +1,10 @@
 cd /cvmfs/fermilab.opensciencegrid.org/products/artdaq/artdaq_spack
 
-./quick-spack-start.sh --padding --no-kmod --arch linux-almalinux9-x86_64_v3
-./ots-quick-spack-start.sh --padding --no-kmod --arch linux-almalinux9-x86_64_v3
+rm quick-spack-start.sh* && wget https://raw.githubusercontent.com/art-daq/artdaq_demo/refs/heads/develop/tools/quick-spack-start.sh && chmod +x quick-spack-start.sh
+rm ots-quick-spack-start.sh* && wget https://raw.githubusercontent.com/art-daq/otsdaq_demo/refs/heads/develop/tools/ots-quick-spack-start.sh && chmod +x ots-quick-spack-start.sh
+
+./quick-spack-start.sh --padding --no-kmod --no-view --arch linux-almalinux9-x86_64_v3
+./ots-quick-spack-start.sh --padding --no-kmod --no-view --arch linux-almalinux9-x86_64_v3
 
 source setup-env.sh
 spack reindex
--- run_update_docker.sh
+++ run_update_docker.sh
@@ -1,3 +1,11 @@
+echo "Starting update/build of CVMFS artdaq_spack area"
+
+rsync -ax --progress /cvmfs/fermilab.opensciencegrid.org/products/artdaq/artdaq_spack/ artdaq_spack/
+
 docker run -it -v ./build_scripts:/opt/build_scripts -v ./artdaq_spack:/cvmfs/fermilab.opensciencegrid.org/products/artdaq/artdaq_spack eflumerf/otsdaq-spack:latest /opt/build_scripts/update_cvmfs.sh
 
 rsync -ax --progress artdaq_spack/ artdaq@artdaqgpvm01:/grid/fermiapp/products/artdaq/artdaq_spack/
+
+ssh artdaq@artdaqgpvm01 ./update_cvmfs.sh
+
+echo "DONE"
