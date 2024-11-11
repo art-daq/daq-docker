60ab2548688643e11ea3079dd94ee7cf30bb9728
Eric Flumerfelt <eflumerf@fnal.gov>
2024-11-11 16:28:45 -0600

    Update build scripts to make a Spack area for each bundle

--- build_scripts/update_cvmfs.sh
+++ build_scripts/update_cvmfs.sh
@@ -1,12 +1,28 @@
-cd /cvmfs/fermilab.opensciencegrid.org/products/artdaq/artdaq_spack
+cd /cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_areas
 
+artVer=132
+artdaqVer=v3_14_01
+otsVer=v2_09_00
+
+function cleanup() {
+    source setup-env.sh
+    spack reindex
+    rm -rf daqlogs daqdata CMakeLists.txt* fonts* log qms-log run_records Data databases script_log DAQInterface Data.bak*
+}
+
+mdkir art-suite-s$artVer && cd art-suite-s$artVer
+rm art-suite-spack-start.sh && https://raw.githubusercontent.com/art-daq/artdaq_demo/refs/heads/develop/tools/art-suite-spack-start.sh && chmod +x art-suite-spack-start.sh
+./art-suite-spack-start.sh --padding --no-view -s $artVer --arch linux-almalinux9-x86_64_v3
+cleanup()
+
+cd /cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_areas
+mkdir artdaq-$artdaqVer && cd artdaq-$artdaqVer
 rm quick-spack-start.sh* && wget https://raw.githubusercontent.com/art-daq/artdaq_demo/refs/heads/develop/tools/quick-spack-start.sh && chmod +x quick-spack-start.sh
+./quick-spack-start.sh --upstream /cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_areas/art-suite-s$artVer --tag $artdaqVer --padding --no-kmod --no-view --arch linux-almalinux9-x86_64_v3
+cleanup()
+
+cd /cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_areas
+mkdir ots-$otsVer && cd ots-$otsVer
 rm ots-quick-spack-start.sh* && wget https://raw.githubusercontent.com/art-daq/otsdaq_demo/refs/heads/develop/tools/ots-quick-spack-start.sh && chmod +x ots-quick-spack-start.sh
-
-./quick-spack-start.sh --padding --no-kmod --no-view --arch linux-almalinux9-x86_64_v3
-./ots-quick-spack-start.sh --padding --no-kmod --no-view --arch linux-almalinux9-x86_64_v3
-
-source setup-env.sh
-spack reindex
-
-rm -rf daqlogs daqdata CMakeLists.txt* fonts* log qms-log run_records Data databases script_log DAQInterface Data.bak*
+./ots-quick-spack-start.sh --padding --no-kmod --no-view --arch linux-almalinux9-x86_64_v3 --tag $otsVer --upstream /cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_areas/art-suite-s$artVer --upstream  /cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_areas/artdaq-$artdaqVer
+cleanup()
--- run_update_docker.sh
+++ run_update_docker.sh
@@ -1,11 +1,13 @@
-echo "Starting update/build of CVMFS artdaq_spack area"
+echo "Starting update/build of CVMFS spack_areas"
 
-rsync -ax --progress /cvmfs/fermilab.opensciencegrid.org/products/artdaq/artdaq_spack/ artdaq_spack/
+mkdir -p spack_areas
 
-docker run -it -v ./build_scripts:/opt/build_scripts -v ./artdaq_spack:/cvmfs/fermilab.opensciencegrid.org/products/artdaq/artdaq_spack eflumerf/otsdaq-spack:latest /opt/build_scripts/update_cvmfs.sh
+rsync -ax --progress /cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_areas/ spack_areas/
 
-rsync -ax --progress artdaq_spack/ artdaq@artdaqgpvm01:/grid/fermiapp/products/artdaq/artdaq_spack/
+docker run -it -v ./build_scripts:/opt/build_scripts -v ./spack_areas:/cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_areas eflumerf/alma9-spack:latest /opt/build_scripts/update_cvmfs.sh
 
-ssh artdaq@artdaqgpvm01 ./update_cvmfs.sh
+#rsync -ax --progress spack_areas/ artdaq@artdaqgpvm01:/grid/fermiapp/products/artdaq/spack_areas/
+
+#ssh artdaq@artdaqgpvm01 ./update_cvmfs.sh
 
 echo "DONE"
