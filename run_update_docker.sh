1f4d8d4390d120555e849de2fc520a37995e75c9
root <root@ironwork.fnal.gov>
2024-12-13 11:10:40 -0600

    Make a separate script for each build type.

--- run_update_docker.sh
+++ run_update_docker.sh
@@ -4,9 +4,11 @@ mkdir -p spack_areas
 
 rsync -ax --progress /cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_areas/ spack_areas/
 
-docker run -it --rm -v ./build_scripts:/opt/build_scripts -v ./spack_areas:/cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_areas eflumerf/alma9-spack:latest /opt/build_scripts/update_cvmfs.sh
+#docker run -it --rm -v ./build_scripts:/opt/build_scripts -v ./spack_areas:/cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_areas eflumerf/alma9-spack:latest /opt/build_scripts/update_cvmfs_art.sh
+docker run -it --rm -v ./build_scripts:/opt/build_scripts -v ./spack_areas:/cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_areas eflumerf/alma9-spack:latest /opt/build_scripts/update_cvmfs_artdaq.sh
+#docker run -it --rm -v ./build_scripts:/opt/build_scripts -v ./spack_areas:/cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_areas eflumerf/alma9-spack:latest /opt/build_scripts/update_cvmfs_ots.sh
 
-rsync -ax --progress spack_areas/ artdaq@artdaqgpvm01:/grid/fermiapp/products/artdaq/spack_areas/
+rsync -ax --delete --progress spack_areas/ artdaq@artdaqgpvm01:/grid/fermiapp/products/artdaq/spack_areas/
 
 ssh artdaq@artdaqgpvm01 ./update_cvmfs.sh
 
