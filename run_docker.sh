ac3a1e44d6221f6db7f7efd718f8c907a93b8144
Eric Flumerfelt <eflumerf@fnal.gov>
2026-02-02 10:29:02 -0600

    Mount whole CVMFS area

--- run_docker.sh
+++ run_docker.sh
@@ -3,4 +3,4 @@
 image=${image:-${1:-eflumerf/alma9-spack:latest}}
 
 docker pull $image
-docker run -it --rm -v ./build_scripts:/opt/build_scripts -v ./spack_areas:/cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_areas $image
+docker run -it --rm -v ./build_scripts:/opt/build_scripts -v ./:/cvmfs/fermilab.opensciencegrid.org/products/artdaq/ $image
