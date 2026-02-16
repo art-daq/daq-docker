0289e696c0a4ada78ddf707952bb0056766b6698
Eric Flumerfelt <eflumerf@fnal.gov>
2026-02-16 11:58:10 -0600

    Set command to run

--- run_docker.sh
+++ run_docker.sh
@@ -2,6 +2,7 @@
 
 os=$(cat /etc/redhat-release|grep -oE "release [0-9]+"|cut -d' ' -f2)
 image=${image:-${1:-eflumerf/alma${os}-spack:latest}}
+cmd=${2:-""}
 
 docker pull $image
-docker run -it --rm -v ./build_scripts:/opt/build_scripts -v ./:/cvmfs/fermilab.opensciencegrid.org/products/artdaq/ $image
+docker run -it --rm -v ./build_scripts:/opt/build_scripts -v ./:/cvmfs/fermilab.opensciencegrid.org/products/artdaq/ $image $cmd
