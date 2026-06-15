88bf583f7123fbd76a6b1d4b545c755981084fe0
Eric Flumerfelt <eflumerf@fnal.gov>
2026-06-15 08:05:01 -0500

    Better rsync of CVMFS areas

--- run_update_docker.sh
+++ run_update_docker.sh
@@ -9,7 +9,7 @@ doArtBuild=1
 doArtdaqBuild=1
 doOtsBuild=1
 doMu2eBuild=1
-update_local=0
+update_local=1
 update_cvmfs=0
 
 checkOnly=0
@@ -21,17 +21,32 @@ forceMu2eBuild=0
 al9Build=$(cat /etc/redhat-release|grep -c "release 9")
 al10Build=$(cat /etc/redhat-release|grep -c "release 10")
 
+if [ ${al9Build:-0} -eq 1 ];then
+  osVer=9
+elif [ ${al10Build:-0} -eq 1 ];then
+  osVer=10
+fi
+
 spack0Build=1
 spack1Build=1
 
 if [ $update_local -eq 1 ];then
-    rsync -ax --progress /cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_v0.28/ spack_v0.28/
-    rsync -ax --progress /cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_v1.1/ spack_v1.1/
+    # art
+    rsync -ax --progress /cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_v0.28/art-suite-$artVer-al$osVer spack_v0.28/
+    rsync -ax --progress /cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_v1.1/art-suite-$artVer-al$osVer spack_v1.1/
+    # artdaq
+    rsync -ax --progress /cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_v0.28/artdaq-$artdaqVer-al$osVer spack_v0.28/
+    rsync -ax --progress /cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_v1.1/artdaq-$artdaqVer-al$osVer spack_v1.1/
+    # ots
+    rsync -ax --progress /cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_v0.28/ots-$otsVer-al$osVer spack_v0.28/
+    rsync -ax --progress /cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_v1.1/ots-$otsVer-al$osVer spack_v1.1/
+    # mu2e
+    rsync -ax --progress /cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_v0.28/mu2e-tdaq-$mu2eVer-al$osVer spack_v0.28/
+    rsync -ax --progress /cvmfs/fermilab.opensciencegrid.org/products/artdaq/spack_v1.1/mu2e-tdaq-$mu2eVer-al$osVer spack_v1.1/
 fi
 
 function build_areas() {
-    osVer=$1
-    spackVer=$2
+    spackVer=$1
     image="eflumerf/alma${osVer}-spack:latest"
 
     docker pull $image
@@ -91,21 +106,11 @@ function build_areas() {
     fi
 }
 
-if [ $al9Build -eq 1 ];then
-    if [ $spack0Build -eq 1 ];then
-        build_areas 9 "v0.28"
-    fi
-    if [ $spack1Build -eq 1 ];then
-        build_areas 9 "v1.1"
-    fi
+if [ $spack0Build -eq 1 ];then
+    build_areas "v0.28"
 fi
-if [ $al10Build -eq 1 ];then
-    if [ $spack0Build -eq 1 ];then
-        build_areas 10 "v0.28"
-    fi
-    if [ $spack1Build -eq 1 ];then
-        build_areas 10 "v1.1"
-    fi
+if [ $spack1Build -eq 1 ];then
+    build_areas "v1.1"
 fi
 
 if [ $update_cvmfs -eq 1 ];then
